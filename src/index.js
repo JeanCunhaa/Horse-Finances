e4d6bb3f17a4400f26f83a8c5134d7e5816ec7d3
Taidaum <taijiuemura2002@hotmail.com>
2022-10-02 08:31:53 -0300

    dados puxados e integração API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React, { useDebugValue } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Api from './Api';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Api />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
