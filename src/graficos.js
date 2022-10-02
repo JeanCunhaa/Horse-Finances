945547684a04d50fa0f2dad4fdf8067f6b66a0cb
Jean Cunha <jean.cunha13@hotmail.com>
2022-10-02 05:13:32 -0300

    testando grafico

---
+++ src/graficos.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales"],
+  ["2013", 1000 ],
+  ["2014", 1170 ],
+  ["2015", 660 ],
+  ["2016", 1030],
+];
+
+export const options = {
+  title: "Bitcoin",
+  hAxis: { title: "Year", titleTextStyle: { color: "#333" } },
+  vAxis: { minValue: 0 },
+  chartArea: { width: "50%", height: "70%" },
+};
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1 className=''>Graficos em React</h1>
+
+      <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+
+    
+    </div>
+  );
+}
+
+export default App;
