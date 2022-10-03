e70abf565cfb6a58574f693b965fcad7cb752f0e
anon4l <ronaldomon02@gmail.com>
2022-10-02 22:49:50 -0300

    Join Header and Principal

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,39 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, useState} from 'react'
 
 import iconeLogin from '../../img/contorno-de-cabeca-de-cavalo.png'
 import iconeLoginSigla from '../../img/HF-branco.png'
 import lupa from '../../img/download.png'
-
-class Header extends Component {
-    render(){
-        return(
-            <>
-                <header>
-                    <div className="area-login">
-                        <div className="icone-cabecalho">
-                            <img src={iconeLogin} className="iconeLogin" />
-                            
-                            <img src={iconeLoginSigla} alt="" className="iconeLoginSigla" />
-                            
-                        </div>
-                        <div className="login">
-                            <a href="" className="link"> Cadastrar</a> |
-                            <a href="" className="link"> Entrar</a>
-                        </div>
-                    </div>
-                    
-                    <div className="header2">
-                        <h1 className="logo">Horse Finances</h1>
-                        <form action="#" method="post">
-                            <input className="search" type="text" placeholder="Buscar" />
-                            <input className="lupa" type="image" src={lupa} />
-                        </form>
-                    </div>
-            
-                </header>
-            </>
-        )
-    }
-}
-
-export default Header
+//
--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import './styles.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
     <Principal />
     <Grafico/>
     <Footer />
