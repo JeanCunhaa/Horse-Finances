8e06279a82c21eff6f26db20304f2303d59f0bf7
anon4l <ronaldomon02@gmail.com>
2022-10-02 23:32:12 -0300

    index de teste

--- src/components/principal/index.js
+++ src/components/principal/index.js
@@ -1,39 +1,9 @@
-import React, { Component,useState } from 'react'
-import iconeLogin from '../../img/contorno-de-cabeca-de-cavalo.png'
-import iconeLoginSigla from '../../img/HF-branco.png'
-import lupa from '../../img/download.png'
-export default function Header() {
+import React, { Component } from 'react'
 
-    const [fname, setFname] = useState("")
-
-    const handleChange = e => {    setFname(e.target.value)  }
+class Principal extends Component {
+    render(){
         return(
             <>
-                <header>
-                    <div className="area-login">
-                        <div className="icone-cabecalho">
-                            <img src={iconeLogin} className="iconeLogin" />
-                            
-                            <img src={iconeLoginSigla} alt="" className="iconeLoginSigla" />
-                            
-                        </div>
-                        <div className="login">
-                            <a href="" className="link"> Cadastrar</a> |
-                            <a href="" className="link"> Entrar</a>
-                        </div>
-                    </div>
-                    
-                    <div className="header2">
-                        <h1 className="logo">Horse Finances</h1>
-                        <form >
-                            <input className="search" type="text" placeholder="Buscar" onChange={handleChange}/>
-                            <input className="lupa" type="image" src={lupa} />
-                        </form>
-                        <button className="botao" >Cadastrar</button>
-                    </div>
-            
-                </header>
-            
                 <div className="principal">
                     <div className="grid-container">
                         <div className="grid-item">
@@ -42,7 +12,7 @@ export default function Header() {
                         <div className="grid-item barra">
                             <div>
                                 <h4>Preço</h4>
-                                <h2 className="precoAtivo"></h2>
+                                <h2 className="precoAtivo">R$1.000.000,00</h2>
                             </div>
                             <div className="variacao">
                                 <h4>Variação(Dia)</h4>
@@ -72,3 +42,6 @@ export default function Header() {
             </>
         )
     }
+}
+
+export default Principal
