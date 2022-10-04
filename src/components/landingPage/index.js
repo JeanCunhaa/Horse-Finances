7858f9833b99678c75770bdcf403cb0e2906b84b
Jean Cunha <jean.cunha13@hotmail.com>
2022-10-04 20:12:49 -0300

    media query

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
 import logo from '../../img/indice.png'
 
+
 class Home extends Component {
     render(){
         return(
             <>
-                <div className='main-login'>
-                    <div className='left-login'>
-                        <h1>Faça login e entre para o nosso time</h1>
-                        <img src={logo} className='logo'/>
-                    </div>
-                    <div className='right-login'>
-                        <div className='card-login'>
-                            <h1>HORSE FINANCES</h1>
-                            <div className='textfield'>
-                                <label for='cadastro'>Para se cadastrar:</label>
-                                <button className='btn-cadastro'>Cadastre-se</button>
-                            </div>
-                            <div className='textfield'>
-                                <label for='entrar'>Ja tem uma conta?</label>
-                                <button className='btn-cadastro'>Entrar</button>
+                <body>
+                    <div className='main-login'>
+                        <div className='left-login'>
+                            <h1>Faça login ou</h1>
+                            
+                            <h1>entre para o nosso time</h1>
+                            <img src={logo} className='logo'/>
+                        </div>
+                        <div className='right-login'>
+                            <div className='card-login'>
+                                <h1>HORSE FINANCES</h1>
+                                <div className='textfield'>
+                                    <label for='cadastro'>Para se cadastrar:</label>
+                                    <button className='btn-cadastro'>Cadastre-se</button>
+                                </div>
+                                <div className='textfield'>
+                                    <label for='entrar'>Ja tem uma conta?</label>
+                                    <button className='btn-cadastro'>Entrar</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </body>
             </>
         )
     }
