2289fabe0d88e3fe7d982139cfa6807b8640c9ca
Jean Cunha <jean.cunha13@hotmail.com>
2022-10-05 06:02:17 -0300

    clima terrivel /3

--- src/pages/Entrar/index.js
+++ src/pages/Entrar/index.js
@@ -2,6 +2,9 @@ import React, { Component,useState } from 'react'
 import iconeLogin from '../../img/contorno-de-cabeca-de-cavalo.png'
 import iconeLoginSigla from '../../img/HF-branco.png'
 
+import { BodyContainer } from './styles'
+import { AreaLoginContainer } from './styles'
+
 //import '../../styleEntrar.css'
 
 
@@ -12,39 +15,41 @@ export default function HeaderEntrar() {
     const handleChange = e => {    setFname(e.target.value)  }
         return(
             <>
-                <header>
-                    <div class="area-login">
-                        <a href="">
-                            <div class="icone-cabecalho">
-                                <img src={iconeLogin} alt="" class="iconeLogin" />
-                            
-                                <img src={iconeLoginSigla} alt="" class="iconeLoginSigla" />
-                            
+                <BodyContainer>
+                    <header>
+                        <AreaLoginContainer>
+                            <a href="">
+                                <div class="icone-cabecalho">
+                                    <img src={iconeLogin} alt="" class="iconeLogin" />
+                                
+                                    <img src={iconeLoginSigla} alt="" class="iconeLoginSigla" />
+                                
+                                </div>
+                            </a>
+                            <div class="login">
+                                <a href="" class="link"> Cadastrar</a> |
+                                <a href="" class="link"> Entrar</a>
                             </div>
-                        </a>
-                        <div class="login">
-                            <a href="" class="link"> Cadastrar</a> |
-                            <a href="" class="link"> Entrar</a>
-                        </div>
+                        </AreaLoginContainer>
+                    </header>
+
+                    <div class="conteudo-principal">
+
+                        <h1>Entrar</h1>
+
+                        <form>
+
+                            <label for="email">E-mail:</label> 
+                            <input type="email" name="email" id="email" /> 
+
+                            <label for="senha">Senha:</label> 
+                            <input type="password" name="senha" id="senha" />
+
+                            <input type="button" value="Entrar" id="entrar" />
+
+                        </form>
                     </div>
-                </header>
-
-                <div class="conteudo-principal">
-
-                    <h1>Entrar</h1>
-
-                    <form>
-
-                        <label for="email">E-mail:</label> 
-                        <input type="email" name="email" id="email" /> 
-
-                        <label for="senha">Senha:</label> 
-                        <input type="password" name="senha" id="senha" />
-
-                        <input type="button" value="Entrar" id="entrar" />
-
-                    </form>
-                </div>
+                </BodyContainer>
             </>
         );
 }
---
+++ src/pages/Entrar/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const BodyContainer = styled.body`
+    margin: 0px;
+    color: #201b2c;
+`;
+
+
+export const AreaLoginContainer = styled.div`
+    
+    align-items: center;
+    display: flex;
+    height: 3rem;
+    justify-content: space-between;
+
+`;
+
